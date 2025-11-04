ef957f68d8dd13b87118924e696570a3f193cc6e
Sufiya Naaz <150449783+Sufiya578@users.noreply.github.com>
2025-11-05 01:47:10 +0530

    Add files via upload
    updated

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -16,7 +16,7 @@ export default function ShopPage() {
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const res = await fetch("https://dummyjson.com/products?limit=100");
+        const res = await fetch("https://dummyjson.com/products?com=100");
         const data = await res.json();
         const list = data.products || [];
         setProducts(list);
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+       
+        //  { protocol: "https", hostname: "api.yourbackend.com" },
+      },
+    ],
+  },
+   reactCompiler: true,
 };
 
 export default nextConfig;
